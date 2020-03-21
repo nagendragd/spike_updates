34f3d80ba4cbc557d3029049208b94918a11f046
Nagendra <ndg0068@aragorn.unt.edu>
2020-03-21 17:06:22 -0500

    timing support

--- riscv/tracer.h
+++ riscv/tracer.h
@@ -4,8 +4,22 @@
 #define _RISCV_TRACER_H
 
 #include "processor.h"
+#include <iostream>
+using namespace std;
 
 static inline void trace_opcode(processor_t* p, insn_bits_t opc, insn_t insn) {
+  bool found = 0;
+  for(int i=0; i<p->lookup_table.size(); i++)
+  {
+    if(opc == p->lookup_table[i].match_opcode)
+    {
+      p->cycles += p->lookup_table[i].delay;
+      std::cout << "Matched timing " << std::endl;
+      found = 1;
+      break;
+    }
+  }
+  if (!found) p->cycles += 1;
 }
 
 #endif
