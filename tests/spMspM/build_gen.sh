12e4ba721661dc111b8deedb5e173f8bf15884f3
Gulur <ndg0068@alveo-cse.unt.ad.unt.edu>
2020-03-27 19:24:58 -0500

    Sparse version prototype

--- tests/spMspM/build_gen.sh
+++ tests/spMspM/build_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 riscv32-unknown-elf-g++ -O3 -fpermissive -I. -c -DGENERATE_ONLY  generate.c
-riscv32-unknown-elf-g++ -O3 -fpermissive -I. -DGENERATE_ONLY  gustavson.c generate.o
+riscv32-unknown-elf-g++ -O3 -fpermissive -I. -DGENERATE_ONLY  spMspM.c generate.o
