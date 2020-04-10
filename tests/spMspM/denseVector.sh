808b151e62286f2b7c9ff6857bf98ab158b5213e
Gulur <ndg0068@alveo-cse.unt.ad.unt.edu>
2020-04-10 18:43:14 -0500

    Dense Vector support

--- tests/spMspM/denseVector.sh
+++ tests/spMspM/denseVector.sh
@@ -4,18 +4,18 @@ rm a.out
 ./build_gen.sh 
 # Run spike iteration #1
 cd ../../build
-./spike --isa=RV32IMCV --varch=v256:e32:s256 ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 0 0 /home/ndg0068/sim/spike/riscv-isa-sim/tests/spmv/temp_dense.dat 0 1 | grep "#define"  > ../tests/spmv/generated_macros.h
+./spike --isa=RV32IMAFDCV --varch=v256:e32:s256 ../../../pk/riscv-pk/build/pk ../tests/spMspM/a.out 0 2 $1 $2 0 1 | grep "#define" > ../tests/spMspM/generated_macros.h
 # Compile iteration #2
-cd ../tests/spmv
+cd ../tests/spMspM
 ./build.sh
 # Run spike iteration #2
 cd ../../build
-./spike --isa=RV32IMCV --varch=v256:e32:s256 ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 0 0 /home/ndg0068/sim/spike/riscv-isa-sim/tests/spmv/temp_dense.dat 0 1 | grep "#define"  > ../tests/spmv/generated_macros.h
+./spike --isa=RV32IMAFDCV --varch=v256:e32:s256 ../../../pk/riscv-pk/build/pk ../tests/spMspM/a.out 0 2 $1 $2 0 1 | grep "#define" > ../tests/spMspM/generated_macros.h
 # Compile iteration #3
-cd ../tests/spmv
+cd ../tests/spMspM
 ./build.sh
 riscv32-unknown-elf-objdump -D a.out > a.dump
 # Run spike iteration #3
 cd ../../build
-./spike --isa=RV32IMCV --varch=v256:e32:s256 ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 1 0 /home/ndg0068/sim/spike/riscv-isa-sim/tests/spmv/temp_dense.dat 0 1
-cd ../tests/spmv
+./spike --isa=RV32IMAFDCV --varch=v256:e32:s256 ../../../pk/riscv-pk/build/pk ../tests/spMspM/a.out 1 2 $1 $2 0 1
+cd ../tests/spMspM
