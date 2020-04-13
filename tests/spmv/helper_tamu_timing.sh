6f99533b4b8600555b0b14059ee8c3293295459d
Gulur <ndg0068@alveo-cse.unt.ad.unt.edu>
2020-04-13 17:08:04 -0500

    invoke spike_spmv

--- tests/spmv/helper_tamu_timing.sh
+++ tests/spmv/helper_tamu_timing.sh
@@ -2,20 +2,20 @@
 rm a.out
 # Compile iteration #1
 ./build_gen.sh 
-# Run spike iteration #1
+# Run spike_spmv iteration #1
 cd ../../build
-./spike --isa=RV32IMAFDCV --varch=v256:e32:s256 --dc=32768:4:32 --l2=131072:8:32 --timing=./timings.txt ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 0 2 $1 1 2 | grep "#define"  > ../tests/spmv/generated_macros.h
+./spike_spmv --isa=RV32IMAFDCV --varch=v256:e32:s256 --dc=32768:4:32 --l2=131072:8:32 --timing=./timings.txt ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 0 2 $1 1 2 | grep "#define"  > ../tests/spmv/generated_macros.h
 # Compile iteration #2
 cd ../tests/spmv
 ./build.sh
-# Run spike iteration #2
+# Run spike_spmv iteration #2
 cd ../../build
-./spike --isa=RV32IMAFDCV --varch=v256:e32:s256 --dc=32768:4:32 --l2=131072:8:32 --timing=./timings.txt ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 0 2 $1 1 2 | grep "#define"  > ../tests/spmv/generated_macros.h
+./spike_spmv --isa=RV32IMAFDCV --varch=v256:e32:s256 --dc=32768:4:32 --l2=131072:8:32 --timing=./timings.txt ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 0 2 $1 1 2 | grep "#define"  > ../tests/spmv/generated_macros.h
 # Compile iteration #3
 cd ../tests/spmv
 ./build.sh
 riscv32-unknown-elf-objdump -D a.out > a.dump
-# Run spike iteration #3
+# Run spike_spmv iteration #3
 cd ../../build
-./spike --isa=RV32IMAFDCV --varch=v256:e32:s256 --dc=32768:4:32 --l2=131072:8:32 --timing=./timings.txt ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 1 2 $1 1 2  
+./spike_spmv --isa=RV32IMAFDCV --varch=v256:e32:s256 --dc=32768:4:32 --l2=131072:8:32 --timing=./timings.txt ../../../pk/riscv-pk/build/pk ../tests/spmv/a.out 1 2 $1 1 2  
 cd ../tests/spmv
